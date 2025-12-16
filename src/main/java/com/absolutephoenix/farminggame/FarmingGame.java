867d9563e1fcc5551d3dcda7facaa911beb15df0
Emberborn <absolutephoenix2017@gmail.com>
2025-12-15 21:35:04 -0500

    initial commit, added LWJGL, and gradle setup, tested that lwjgl is working correctly.

---
+++ src/main/java/com/absolutephoenix/farminggame/FarmingGame.java
@@ -0,0 +1,9 @@
+package com.absolutephoenix.farminggame;
+
+import org.lwjgl.Version;
+
+public class FarmingGame {
+    public static void main(String[] args){
+        System.out.println("Hello LWJGL" + Version.getVersion() + "!");
+    }
+}
